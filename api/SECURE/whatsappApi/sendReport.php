665f2df19a0b6abc9cabf966fe0108b7fa56d83f
JStack Innovations <wsamson630@gmail.com>
2026-03-31 22:02:45 +0100

    Remove CORS headers from sendReport.php
    Removed CORS headers from sendReport.php.

--- api/SECURE/whatsappApi/sendReport.php
+++ api/SECURE/whatsappApi/sendReport.php
@@ -1,7 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type");
+require_once __DIR__ . "/../authGuard.php";
 
 require __DIR__ . '/vendor/autoload.php';
 use Twilio\Rest\Client;
