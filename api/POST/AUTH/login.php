c8bd1b8b8aaa91162ee16c404bf3c1dc7a114e54
JStack Innovations <wsamson630@gmail.com>
2026-04-27 12:27:22 +0100

    Toggle require statements for mailer files

--- api/POST/AUTH/login.php
+++ api/POST/AUTH/login.php
@@ -1,8 +1,8 @@
 <?php
 
-require_once __DIR__ . '/../../SECURE/gmailApi/gmail_mailer.php';
+//require_once __DIR__ . '/../../SECURE/gmailApi/gmail_mailer.php';
 
-//require_once __DIR__ . '/../../SECURE/gmailApi/resend_mailer.php';
+require_once __DIR__ . '/../../SECURE/gmailApi/resend_mailer.php';
 
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, OPTIONS");
