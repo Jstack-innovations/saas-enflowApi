b4ff4997e769e55721b79428024f4aaa30f31f4a
jStack●Innovations <wsamson630@gmail.com>
2026-05-29 07:58:43 +0100

    Update getKdsMenu.php

--- api/admins/GET/getKdsMenu.php
+++ api/admins/GET/getKdsMenu.php
@@ -1,6 +1,9 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') { http_response_code(200); exit; }
 
-//require_once __DIR__ . "/../../SECURE/authGuard.php";
 require_once __DIR__ . "/../../SECURE/db.php";
 
 $menuFile = __DIR__ . "/../../GET/JSON/menu.json";
