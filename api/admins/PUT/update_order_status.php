79442606ae12f42497d0e05194e2e801f04f734d
jStack●Innovations <wsamson630@gmail.com>
2026-06-13 08:34:15 +0100

    Update update_order_status.php

--- api/admins/PUT/update_order_status.php
+++ api/admins/PUT/update_order_status.php
@@ -1,5 +1,5 @@
 <?php
-//require_once __DIR__ . "/../../SECURE/authGuard.php";
+require_once __DIR__ . "/../../SECURE/authGuard.php";
 require_once __DIR__ . '/../../SECURE/config.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'PUT') {
