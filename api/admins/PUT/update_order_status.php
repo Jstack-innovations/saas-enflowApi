be8de2da9dc4c05a42c1818adc206853af329d06
jStack●Innovations <wsamson630@gmail.com>
2026-06-13 08:17:50 +0100

    Update update_order_status.php

--- api/admins/PUT/update_order_status.php
+++ api/admins/PUT/update_order_status.php
@@ -1,22 +1,24 @@
 <?php
 //require_once __DIR__ . "/../../SECURE/authGuard.php";
-
 require_once __DIR__ . '/../../SECURE/config.php';
-
 
 if ($_SERVER['REQUEST_METHOD'] === 'PUT') {
 
-parse_str(file_get_contents("php://input"), $putData);
-$id = $putData['id'] ?? '';
-$status = $putData['status'] ?? '';
-    
-$stmt = $pdo->prepare("
-    UPDATE paid_orders 
-    SET order_status = ? 
-    WHERE order_id = ?
-");
+    parse_str(file_get_contents("php://input"), $putData);
 
-$stmt->execute([$status, $id]);
+    $id = $putData['id'] ?? '';
+    $status = $putData['status'] ?? '';
 
-    echo json_encode(['success' => true]);
+    $stmt = $pdo->prepare("
+        UPDATE paid_orders
+        SET order_status = ?
+        WHERE id = ?
+    ");
+
+    $stmt->execute([$status, $id]);
+
+    echo json_encode([
+        'success' => true,
+        'rows' => $stmt->rowCount()
+    ]);
 }
