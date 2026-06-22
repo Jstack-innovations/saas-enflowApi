41f4794d563bc4b23fe4a09129a045872aead2fd
jStack●Innovations <wsamson630@gmail.com>
2026-06-22 01:02:06 +0100

    Fix typo in CENTRAL_SERVER URL

--- api/SECURE/centralProxy.php
+++ api/SECURE/centralProxy.php
@@ -1,3 +1,3 @@
 <?php
 // /SECURE/central.php
-const CENTRAL_SERVER = "https://enflowsubscriptions-production.up.railway.app";
+const CENTRAL_SERVER = "https://enflowsubscriptions-production.up.railway.appe";
